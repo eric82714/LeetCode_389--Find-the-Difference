c45caad9e2d3dc8be177dd0514508126f4694927
eric82714 <0958820714ab@gmail.com>
2020-03-27 08:29:10 +0800

    Create find_the_difference.cpp

---
+++ C++/find_the_difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int s_sum = 0, t_sum = 0;
+	    
+	    for(int i = 0; i < s.length(); i++)
+		    s_sum += s[i];
+	    
+	    for(int i = 0; i < t.length(); i++)
+		    t_sum += t[i];
+	    
+        return (char)(t_sum-s_sum);
+    }
+};
